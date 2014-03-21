b8262420532ec4637e63b2f7853cd19a69b0e4f0
Steven Gleed <ledbybyson@gmail.com>
2014-03-21 11:33:42 +0000

    Initial code commit

---
+++ 1427/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PDTUtils
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            RowOne.Height = new GridLength(75);
+            ColumnOne.Width = new GridLength(200);
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnHoppers_Click(object sender, RoutedEventArgs e)
+        {
+            TabControl tb = new TabControl();
+        }
+    }
+}
